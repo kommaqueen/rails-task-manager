ba589d409bab2c92e06a47d6bf22fe96bbb16347
Deanna Wright <deanna.j.wright@gmail.com>
2023-01-21 15:13:44 +0100

    somewhat styled but crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :tasks
+  # get "/tasks", to: "tasks#index"
+  # get "/tasks/new", to: "tasks#new", as: :new_task
+  # get "/tasks/:id", to: "tasks#show", as: :task
+  # post "/tasks", to: "tasks#create"
+
+  # get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "/tasks/:id", to: "tasks#update"
+
+  # delete "tasks/:id", to: "tasks#delete"
 end
